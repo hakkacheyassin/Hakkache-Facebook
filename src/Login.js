23baf5656fdcc041353f767928c2933f92c1d2b5
Hakkache Yassine <35065372+hakkache06@users.noreply.github.com>
2020-10-15 13:24:08 +0100

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,7 @@
 import { Button } from '@material-ui/core';
 import React from 'react';
 import './Login.css';
-import { auth, provider } from './firebase'
+import { auth, provider } from './firebase';
 
 
 
